22d3c3146f973abe67dbffd38a2eb82dd4b40732
RhukananiNkuna <rhukanani.m.nkuna@gmail.com>
2023-02-16 11:32:05 -0800

    Q6 commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main - A program to print out a string using std output
  * Return: 0 (Sucess)
  */
-int mian(void)
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
